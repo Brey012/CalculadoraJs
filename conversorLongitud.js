e78b68b0bcf1be4d875ceb4613fb74a63fe3ca06
Breyner012 <bhernandezos@cesde.net>
2024-11-07 16:20:41 -0500

    Creacion de funciones

--- conversorLongitud.js
+++ conversorLongitud.js
@@ -2,5 +2,12 @@
 function metrosAKm() {
     let metros = prompt("Ingrese los metros  a convertir");
     let km = metros / 1000;
-    // console.log(km)
+    alert("Los " + metros + " metros son " + km + " kilómetros");
 } 
+
+function kmAMillas() {
+    let km = prompt("Ingrese los kilómetros a convertir");
+    let millas = km * 0.621371;
+    alert("Los " + km + " kilómetros son " + millas + " millas");
+}
+
